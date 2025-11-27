52938f553f9f9070d47ac371187f5e43ea38e61a
MDErik <erik.mareniusdehlin@elev.ga.ntig.se>
2025-11-27 15:42:53 +0100

    askdjmpasjd

--- app.rb
+++ app.rb
@@ -9,16 +9,33 @@ require 'sinatra/reloader'
 
 # Routen /
 get '/todo' do
-    db = SQLite3::Database.new("db/todos.db")
+    @db = SQLite3::Database.new("db/todos.db")
+    @db.results_as_hash = true
 
-    db.results_as_hash = true
-
-
-
+    @todos = @db.execute("SELECT * FROM todos WHERE done = 0")
+    @done = @db.execute("SELELCT * FROM todos WHERE done = 1")
 
     
-
     slim(:index)
 end
 
+post '/todo' do
+    title = params[:title]
+    description = params[:description]
+    category = params[:category]
+
+    @db.execute(
+        "INSERT INTO todos (title, description, category) VALUES (?, ?, ?)",[title, description, category]
+    )
+
+end
+
+post "/todo/:id/toggle" do 
+    id = params[:id]
+    
+    todo = @db.execute("SELECT done FROM todos WHERE id = ?", [id]).first
+    new_value = todo["done"] == 1 ? 0 : 1
+
+
+end
 
